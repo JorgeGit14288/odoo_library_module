df05edb8e8cf86cdba0e919077927ff612911509
Jorge Fuentes <jorge.fuentes.14288@gmail.com>
2018-09-26 15:23:19 -0600

    se agrego un modulo de hospital a manera de referencia

--- tienda/__manifest__.py
+++ tienda/__manifest__.py
@@ -25,7 +25,8 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/views_usuario.xml',
+        # 'views/views_usuario.xml',
+        'views/views_personalizada.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- tienda/models/model_tienda.py
+++ tienda/models/model_tienda.py
@@ -8,6 +8,8 @@ class categoria(models.Model):
         string='codigo de categoria'
     )
     nombre=fields.Char(string="Nombre de la Categoria",required=True,size=20)
+    descripcion = fields.Html(string="Descripcion de la categoria")
+    imagen=fields.Binary(string="Imagen de Categoria")
 
     @api.constrains('nombre')
     def check_name(self):
